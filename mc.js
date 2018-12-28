51af3afb1fa965dd59e1f56e957351fb40983449
dcook <dcook0819@gmail.com>
2018-12-27 22:19:00 -0800

    Crouched only moves half a block

--- mc.js
+++ mc.js
@@ -436,8 +436,9 @@ setInterval(drawEverything, 20);
 
 r.onKeyDown(function(key) {
     console.log("KEY: " + key);
-    if (key === 37 && person.x > 0                   ) { person.x = person.x - 1; } // LEFT
-    if (key === 39 && person.x < blocks[0].length - 1) { person.x = person.x + 1; } // RIGHT
+    var blocksToMove = (person.crouched) ? 0.5 : 1;
+    if (key === 37 && person.x > 0                   ) { person.x = person.x - blocksToMove; } // LEFT
+    if (key === 39 && person.x < blocks[0].length - 1) { person.x = person.x + blocksToMove; } // RIGHT
     if (key === 40) { person.crouched = !(person.crouched); } // DOWN
     if (key === 32) { dig(); } // SPACE
     if (key === 16) { showCoords = !showCoords; } // SHIFT
